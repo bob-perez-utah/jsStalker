458d8881b894423404ec838cd417a2ebbb3c9ece
bob-perez-utah <bperez@gw.novell.com>
2018-08-19 06:57:57 -0600

    final commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var first = prompt("What is your first name ? : ");
+var last  = prompt("What is your last name ? : ");
+var age   = prompt("What is your age ? : ");
+
+console.log("Your name is " + first + " " + last);
+console.log("and your age is " + age);
